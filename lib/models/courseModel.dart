c081d905976cd3f11c68e4c2f6da4a32e245effe
Bharath Ajay <40299420+bharathajay51@users.noreply.github.com>
2020-07-06 23:57:14 +0530

    Replaced difficulty with description

--- lib/models/courseModel.dart
+++ lib/models/courseModel.dart
@@ -2,13 +2,13 @@ class Course {
   String imgUrl;
   String name;
   String duration;
-  String difficulty;
+  String description;
 
   Course({
     this.imgUrl,
     this.name,
     this.duration,
-    this.difficulty,
+    this.description,
   });
 }
 
@@ -17,24 +17,24 @@ List<Course> courses = [
     imgUrl: 'assets/images/python.png',
     name: 'Python',
     duration: '3 weeks',
-    difficulty: 'Introduction to python programming. Learn to code in the powerful language',
+    description: 'Introduction to python programming. Learn to code in the powerful language',
   ),
   Course(
     imgUrl: 'assets/images/arduino.png',
     name: 'Arduino',
     duration: '4 weeks',
-    difficulty: 'Introduction to python programming. Learn to code in the powerful language',
+    description: 'Introduction to python programming. Learn to code in the powerful language',
   ),
   Course(
     imgUrl: 'assets/images/js.png',
     name: 'JavaScript',
     duration: '4 weeks',
-    difficulty: 'Introduction to python programming. Learn to code in the powerful language',
+    description: 'Introduction to python programming. Learn to code in the powerful language',
   ),
   Course(
     imgUrl: 'assets/images/html.png',
     name: 'Web Dev',
     duration: '5 Weeks',
-    difficulty: 'Introduction to python programming. Learn to code in the powerful language',
+    description: 'Introduction to python programming. Learn to code in the powerful language',
   ),
 ];
