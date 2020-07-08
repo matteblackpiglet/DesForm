0ee1c26c7a8501c4677906b289d9e157120c6998
SRJ <managesrj@gmail.com>
2020-07-08 10:57:46 +0530

    Fixed the Profile Screen issue.

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -33,68 +33,68 @@ class _ProfilePageState extends State<ProfilePage> {
 
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.only(bottom: 30.0),
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: FractionalOffset.topCenter,
-                  end: FractionalOffset.bottomCenter,
-                  colors: [
-                    Theme.of(context).primaryColorLight,
-                    Theme.of(context).primaryColor
-                  ]
-                ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(bottom: 30.0),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: FractionalOffset.topCenter,
+                end: FractionalOffset.bottomCenter,
+                colors: [
+                  Theme.of(context).primaryColorLight,
+                  Theme.of(context).primaryColor
+                ]
               ),
-              child: SafeArea(
-                child: SizedBox(
-                  height: scaler.getHeight(10.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.only(left: 30.0, bottom: 16.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: <Widget>[
-                            ProfilePhotoPlain(
-                              url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQM--hVamcJhFyQcqARua0LV9c-opvPKdxCZw&usqp=CAU'
-                            ),
-                            Container(
-                              margin: EdgeInsets.only(left: 16.0),
-                              child: Heading(
-                                text: 'X Æ A-12',
-                                color: Color(0xffffffff),
-                                weight: FontWeight.w700,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            ),
+            child: SafeArea(
+              child: SizedBox(
+                height: scaler.getHeight(10.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(left: 30.0, bottom: 16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
                         children: <Widget>[
-                          StatusCard(
-                            text: 'Ongoing Courses',
-                            count: '3',
+                          ProfilePhotoPlain(
+                            url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQM--hVamcJhFyQcqARua0LV9c-opvPKdxCZw&usqp=CAU'
                           ),
-                          StatusCard(
-                            text: 'Courses Finished',
-                            count: '1',
+                          Container(
+                            margin: EdgeInsets.only(left: 16.0),
+                            child: Heading(
+                              text: 'X Æ A-12',
+                              color: Color(0xffffffff),
+                              weight: FontWeight.w700,
+                            ),
                           ),
                         ],
                       ),
-                    ],
-                  ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        StatusCard(
+                          text: 'Ongoing Courses',
+                          count: '3',
+                        ),
+                        StatusCard(
+                          text: 'Courses Finished',
+                          count: '1',
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
             ),
-            Container(
+          ),
+          Expanded(
+            child: Container(
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.only(topLeft: Radius.circular(mainCurve), topRight: Radius.circular(mainCurve)),
                 color: Color(0xffffffff),
@@ -108,58 +108,55 @@ class _ProfilePageState extends State<ProfilePage> {
               ),
               child: Column(
                 children: <Widget>[
-                  SizedBox(
-                    height: scaler.getHeight(24.7),
-                    child: Container(
-                      margin: EdgeInsets.only(top: 20.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          Text(
-                            'ABOUT',
-                            style: TextStyle(
-                              color: Theme.of(context).primaryColor,
-                              fontFamily: 'Montserrat',
-                              fontWeight: FontWeight.w600,
-                              fontSize: scaler.getTextSize(8.0)
-                            ),
+                  Container(
+                    margin: EdgeInsets.only(top: 20.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          'ABOUT',
+                          style: TextStyle(
+                            color: Theme.of(context).primaryColor,
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.w600,
+                            fontSize: scaler.getTextSize(8.0)
                           ),
-                          Container(
-                            margin: EdgeInsets.symmetric(horizontal: 30.0),
-                            child: Column(
-                              children: <Widget>[
-                                Tag(
-                                  title: 'Name:',
-                                  value: 'X Æ A-12',
-                                ),
-                                Tag(
-                                  title: 'User ID:',
-                                  value: 'xaea12',
-                                ),
-                                Tag(
-                                  title: 'DOB:',
-                                  value: '12-2-1994',
-                                ),
-                                Tag(
-                                  title: 'Sex:',
-                                  value: 'Male',
-                                ),
-                                Tag(
-                                  title: 'Email:',
-                                  value: 'XÆA12@gmail.com',
-                                ),
-                              ]
-                            ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.symmetric(horizontal: 30.0),
+                          child: Column(
+                            children: <Widget>[
+                              Tag(
+                                title: 'Name:',
+                                value: 'X Æ A-12',
+                              ),
+                              Tag(
+                                title: 'User ID:',
+                                value: 'xaea12',
+                              ),
+                              Tag(
+                                title: 'DOB:',
+                                value: '12-2-1994',
+                              ),
+                              Tag(
+                                title: 'Sex:',
+                                value: 'Male',
+                              ),
+                              Tag(
+                                title: 'Email:',
+                                value: 'XÆA12@gmail.com',
+                              ),
+                            ]
                           ),
-                        ]
-                      ),
+                        ),
+                      ]
                     ),
                   ),
                 ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
